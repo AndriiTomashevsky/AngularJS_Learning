bc8f1faabe0fc4ba9a24d1db635cc090d69fbc5a
Andrii Tomashevskyi <tomashevskiy.a@gmail.com>
2019-02-09 17:17:01 +0200

    Create round-trip application

---
+++ AngularJS/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using AngularJS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularJS.Controllers
+{
+    public class HomeController : Controller
+    {
+        ReservationRepository reservationRepository = ReservationRepository.Singleton();
+
+        public HomeController()
+        {
+        }
+
+        public ActionResult Index()
+        {
+            return View(reservationRepository.Reservations);
+        }
+
+        public ActionResult Add(Reservation reservation)
+        {
+            if (ModelState.IsValid)
+            {
+                reservationRepository.Add(reservation);
+
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(reservationRepository.Reservations);
+            }
+        }
+
+        public ActionResult Remove(int reservationId)
+        {
+            reservationRepository.Remove(reservationId);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ AngularJS/Models/ReservationRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AngularJS.Models
+{
+    public class ReservationRepository
+    {
+        static ReservationRepository reservationRepository = new ReservationRepository();
+
+        static List<Reservation> reservations = new List<Reservation>()
+        {
+            new Reservation(){ ReservationId=1,ClientName="Adam",Location="Board Room"},
+            new Reservation(){ReservationId=2,ClientName="Jacqui",Location="Lecture Hall"},
+            new Reservation(){ ReservationId=3,ClientName="Russell",Location="Meeting Room 1"}
+        };
+
+        public static ReservationRepository Singleton()
+        {
+            return reservationRepository;
+        }
+
+        internal void Add(Reservation reservation)
+        {
+            reservation.ReservationId = reservations.Count + 1;
+            reservations.Add(reservation);
+        }
+
+        internal void Remove(int reservationId)
+        {
+            Reservation deletedReservation = reservations.Find(r => r.ReservationId == reservationId);
+            reservations.Remove(deletedReservation);
+        }
+
+        public IEnumerable<Reservation> Reservations
+        {
+            get { return reservations; }
+        }
+    }
+}
